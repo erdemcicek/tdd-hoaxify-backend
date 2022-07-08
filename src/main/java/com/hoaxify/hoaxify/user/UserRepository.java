752eda254cb6b969e4c25fdde56993139346a020
erdemcicek <erdem25123@gmail>
2022-07-08 04:12:26 +0200

    signup page until validation backend

---
+++ src/main/java/com/hoaxify/hoaxify/user/UserRepository.java
@@ -0,0 +1,13 @@
+package com.hoaxify.hoaxify.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	//List<User> findByUsernameContaining(String username);
+	
+	//User findByUsernameAndDisplayName(String username, String displayName);
+	
+	User findByUsername(String username);
+	
+}
