cf50c6baf9f08655db0bd93149db3167afae4be9
erdemcicek <erdem25123@gmail>
2022-07-27 02:54:50 +0200

    until userupdate backend

--- src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
+++ src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
@@ -346,6 +346,34 @@ public class UserControllerTest {
 
 	}
 	
+	@Test
+	public void getUserByUsername_whenUserExist_receiveOk() {
+		String username = "test-user";
+		userService.save(TestUtil.createValidUser(username));
+		ResponseEntity<Object> response = getUser(username, Object.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+	
+	@Test
+	public void getUserByUsername_whenUserExist_receiveUserWithoutPassword() {
+		String username = "test-user";
+		userService.save(TestUtil.createValidUser(username));
+		ResponseEntity<String> response = getUser(username, String.class);
+		assertThat(response.getBody().contains("password")).isFalse();
+	}
+	
+	@Test
+	public void getUserByUsername_whenUserDoesNotExist_receiveNotFound() {
+		ResponseEntity<Object> response = getUser("unknown-user", Object.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	}
+	
+	@Test
+	public void getUserByUsername_whenUserDoesNotExist_receiveApiError() {
+		ResponseEntity<ApiError> response = getUser("unknown-user", ApiError.class);
+		assertThat(response.getBody().getMessage().contains("unknown-user")).isTrue();
+	}
+	
 	private void authenticate(String username) {
 		testRestTemplate.getRestTemplate().getInterceptors().add(new BasicAuthenticationInterceptor(username, "P4ssword"));
 	}
@@ -361,6 +389,11 @@ public class UserControllerTest {
 	
 	public <T> ResponseEntity<T> getUsers(String path, ParameterizedTypeReference<T> responseType) {
 		return testRestTemplate.exchange(path, HttpMethod.GET, null, responseType);
+	}
+	
+	public <T> ResponseEntity<T> getUser(String username, Class<T> responseType){
+		String path = new StringBuilder(API_1_0_USERS).append("/").append(username).toString();
+		return testRestTemplate.getForEntity(path, responseType);
 	}
 
 	
